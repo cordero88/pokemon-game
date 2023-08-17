5f9d375c0ef9ff9bc5a8b59445115b06e9295166
cordero88 <cordero.angel88@gmail.com>
2023-08-17 17:28:52 -0600

    First commit

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,26 @@
+<script lang = "ts">
+      import Pokemonoptions from '@/components/PokemonOptions.vue';
+      import PokemonPicture from '@/components/PokemonPicture.vue';
+
+      export default {
+      components:{ Pokemonoptions,PokemonPicture}
+      }
+
+</script>
+
+
+<template>
+
+    <h1>¿Quien es este pokémon?</h1>
+    <PokemonPicture :pokemonId="93" :showPokemon="false"/>
+    <Pokemonoptions/>
+
+
+</template>
+
+
+<style>
+
+
+
+</style>
